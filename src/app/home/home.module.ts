d3d51674d83507fe846343ca5956ad08bb10d52a
Earl Hans Genoso <earlhansg@gmail.com>
2020-02-17 19:09:28 +0800

    #1 Create Simple UI

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,7 +4,10 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-import { HomePage } from './home.page';
+import { HomePage } from './containers/home/home.page';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   imports: [
@@ -18,6 +21,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, HeaderComponent, SearchComponent, MapComponent]
 })
 export class HomePageModule {}
